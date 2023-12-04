21279e2bd6008a91bf84528512153a0533ccbf6d
Jack Bisceglia <jackbisceglia2000@gmail.com>
2023-12-03 19:13:52 -0500

    fix: pnpm/npm issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,14 @@ import '../app.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../components/layout/Layout';
-import localFont from '@next/font/local';
+import localFont from 'next/font/local';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
+      {/* <Head>
         <html lang="en" />
-      </Head>
+      </Head> */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
