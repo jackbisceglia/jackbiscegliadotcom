436e7d11f4ae8643948b42b149022445aaed44bf
Jack Bisceglia <jackbisceglia2000@gmail.com>
2023-12-03 19:22:22 -0500

    fix: put lang in next config and remove Head from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,12 @@ import '../app.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../components/layout/Layout';
-import localFont from 'next/font/local';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <html lang="en" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
